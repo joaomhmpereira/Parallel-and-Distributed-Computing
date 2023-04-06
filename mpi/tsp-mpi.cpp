1255395393acc8c363bd73bad2a2c5ae40b5120a
João Maria Henriques Madeira Pereira <joaompereira07@gmail.com>
2023-04-06 15:38:29 +0100

    fix receive

--- mpi/tsp-mpi.cpp
+++ mpi/tsp-mpi.cpp
@@ -289,13 +289,16 @@ void tsp(double * best_tour_cost, int max_value, int n_cities, int ** best_tour,
                 fprintf(stderr, "[TASK %d] Sending token to %d : token color -> %d \n", id, next_rank, token);
                 MPI_Send(&token, 1, MPI_INT, next_rank, TOKEN_TAG, MPI_COMM_WORLD);
                 /* P0 is waiting for a token from P(n_tasks - 1) */
-                MPI_Irecv(&token, 1, MPI_INT, prev_rank, TOKEN_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE, &request);
-                fprintf(stderr, "[TASK %d] Received token from %d : token color -> %d \n", id, prev_rank, token);
-                /* if P0 receives a black token, it will pass a white token */
-                if (token == BLACK) token = WHITE;
-                /* if P0 receives a white token, computation can terminate */
-                else {
-                    terminate = 0;
+                MPI_Irecv(&token, 1, MPI_INT, prev_rank, TOKEN_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE, &request_receive);
+                MPI_Test(&request_receive, &flag, MPI_STATUS_IGNORE);
+                if (flag) {
+                    fprintf(stderr, "[TASK %d] Received token from %d : token color -> %d \n", id, prev_rank, token);
+                    /* if P0 receives a black token, it will pass a white token */
+                    if (token == BLACK) token = WHITE;
+                    /* if P0 receives a white token, computation can terminate */
+                    else {
+                        terminate = 0;
+                    }
                 }
             }
             else {
